fb0a8f686683d34c25dd8b3963eef378dfca0b8d
morzhik02 <marzhanizteleuova@gmail.com>
2021-11-02 00:39:46 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Tea myTea = new Tea();
+        myTea.prepareRecipe();
+
+        System.out.println("\n");
+
+        Coffee myCoffee = new Coffee();
+        myCoffee.prepareRecipe();
+    }
+}
